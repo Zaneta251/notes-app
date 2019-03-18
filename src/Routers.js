bb3f3522c156a8a2a9fddf8ba7cbfab3e0870e18
zaneta.kochanikova <zaneta.kochanikova@accenture.com>
2019-03-18 13:46:04 +0100

    router change

--- src/Routers.js
+++ src/Routers.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Main from "./containers/main/Main";
 import Redirect from "./containers/redirect/Redirect";
 import history from "./routerHistory";
@@ -8,13 +8,13 @@ import history from "./routerHistory";
 export default class Routers extends React.Component {
   render() {
     return (
-      <Router history={history}>
+      <HashRouter history={history}>
         <Switch>
           <Route exact path="/" render={props => <Main {...props} />} />
           <Route exact path="/redirect" render={props => <Redirect {...props} />} />
           <Route render={() => <h1>Page not found</h1>} />
         </Switch>
-      </Router>
+      </HashRouter>
     );
   }
 }
